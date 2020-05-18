463ae47c09228f6620f2238c0f2510a5cecbdb8c
Robin <hello@robinschuettert.com>
2020-05-18 14:21:11 +0200

    feat: changed opening times

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,14 +15,13 @@ const IndexPage = () => (
       <article className="welcomeintro">
         <div>
         <p>Kneipencafé & Coworking in der<br/><br/><a href="https://goo.gl/maps/XAzSkJpCaSU87U6d9">Anzengruberstr 3<br/>12043 Berlin</a><br/><br/>Öffnungszeiten:<br/>
-Di&thinsp;&&thinsp;Mi: 10&thinsp;&ndash;&thinsp;00&thinsp;Uhr<br/>
-Do&thinsp;&&thinsp;Fr: 10&thinsp;&ndash;&thinsp;02&thinsp;Uhr<br/>
-Sa: 14&thinsp;&ndash;&thinsp;02&thinsp;Uhr<br/>
-So&thinsp;&&thinsp;Mo: Ruhetage</p>
+Mi&thinsp;bis&thinsp;Fr: 09&thinsp;&ndash;&thinsp;22&thinsp;Uhr<br/>
+Sa: 11&thinsp;&ndash;&thinsp;22&thinsp;Uhr<br/>
+</p>
         </div>
       </article>
       <article className="contactintro">
-        <p><a href="mailto:anzengruberinnen@gmail.com">anzengruberinnen@gmail.com</a><br/><a href="https://www.instagram.com/anzengruberin/">Instagram</a>, <a href="https://www.facebook.com/anzengruberin/">Facebook</a></p>
+        <p><a href="mailto:hallo@anzengruber.in">hallo@anzengruber.in</a><br/><a href="https://www.instagram.com/anzengruberin/">Instagram</a>, <a href="https://www.facebook.com/anzengruberin/">Facebook</a></p>
       </article>
     </section>
   </Layout>
